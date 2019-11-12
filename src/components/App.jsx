094e3c85e6f0aa664a0dfd8c78468797f66649e6
Student <example@example.com>
2019-11-12 15:46:36 -0800

    practice react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,13 @@ import TicketList from "./TicketList";
 import Header from "./Header";
 
 
-function App(){
-    return (
-        <div>
-          <Header/>
-          <TicketList/>
-        </div>
-      );
-  }
-  
-  export default App;
+function App() {
+  return (
+    <div>
+      <Header />
+      <TicketList />
+    </div>
+  );
+}
+
+export default App;
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -2,23 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 
 
-function Ticket(props){
-    return (
-      <div>
-           <style jsx>{`
+function Ticket(props) {
+  return (
+    <div>
+      <style jsx>{`
           div {
             background-color: red;
           }
         `}</style>
-        <h3>{props.location} - {props.names}</h3>
-        <p><em>{props.issue}</em></p>
-        <hr/>
-      </div>
-    );
-  }
-  
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr />
+    </div>
+  );
+}
+
 Ticket.propTypes = {
-    names: PropTypes.string,
+  names: PropTypes.string,
   location: PropTypes.string,
   issue: PropTypes.string
 };

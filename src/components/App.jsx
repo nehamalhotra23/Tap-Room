d85dd89bb0f3e1f220bc5ebf6a3b56900da13c80
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-11-15 08:31:44 -0800

    add two components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Home from './Home';
-import AlienList from "./AlienList";
+import Header from './Header';
+import KombuchaList from "./KombuchaList";
 import { Switch, Route } from 'react-router-dom';
 
 
@@ -10,12 +10,13 @@ function App() {
   }
   return (
     <div style={color}>
+     
        <Switch>
-       <div className="row">
-       <div className="col-md-12"><Route exact path="/" component={Home} /></div>
+        
+       <div ><Route exact path="/" component={Header} /></div>
        
-        <div className="col-md-7"><Route exact path="/AlienList" component={AlienList} /></div>
-        </div>
+        <div ><Route exact path="/KombuchaList" component={KombuchaList} /></div>
+        
       </Switch>
        </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,13 @@
 import React from "react";
-import cover from '../Assets/Img/cover.jpg';
-
 
 function Header() {
-  var coverPic = {
-    width: "100%",
-    height: "50%",
-    backgroundPosition:"center",
-    backgroundRepeat:" no-repeat",
-    backgroundSize: "cover",
-    overflow: "hidden",
-    position: "relative",
-    top: "0",
-    left: "0",
 
-  }
- 
-
-  var heroText = {
-  textAlign: "center",
-  position: "absolute",
-  top:"5%",
-  left: "40%",
-  opacity: "0.6",
-  color: "white",
-  fontSize: "80px",
- 
-  }
   return (
     <div>
-      <div className="hero-image">
-        <div className="hero-text">
-          <img src={cover} style={coverPic} />
-          <h1  style={heroText}>Square Space</h1>
+          <h1>Kombucha</h1>
         </div>
-      </div>
-    </div>
+     
+   
   );
 }
 
